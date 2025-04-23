e08e4e296ffda6ee35243f75103dec7531a5e1f2
Emanuelle Machado <manumachado03@outlook.com>
2025-04-22 22:12:13 -0300

    (Add) Bill activity layout
    Adding the layout for the view that will list the Bills

---
+++ app/src/main/java/com/example/controle_de_contas/TelaConta.java
@@ -0,0 +1,34 @@
+package com.example.controle_de_contas;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class TelaConta extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_tela_conta);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    public void adicionar(View v){
+
+    }
+
+    public void voltar(View v){
+
+    }
+
+}
