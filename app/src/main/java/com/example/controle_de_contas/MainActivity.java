8d81479e38952d69d9d8670ed59cc574ea324685
Emanuelle Machado <manumachado03@outlook.com>
2025-04-23 21:17:15 -0300

    (Add) Simple connection between activitys
    Activity result launcher and intents added to go to one activity to another. Also prepared for the exchange of information.

--- app/src/main/java/com/example/controle_de_contas/MainActivity.java
+++ app/src/main/java/com/example/controle_de_contas/MainActivity.java
@@ -1,15 +1,33 @@
 package com.example.controle_de_contas;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AbsListView;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.Date;
+import java.util.LinkedList;
+
 public class MainActivity extends AppCompatActivity {
+    LinkedList<Categoria> categorias;
+    ArrayAdapter<Categoria> adapter;
+    ListView lista;
+    int posEdicao = -1;
+    ActivityResultLauncher<Intent> listagem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +39,53 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        categorias = new LinkedList<>();
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, categorias);
+        lista = findViewById(R.id.lista_categorias);
+        lista.setAdapter(adapter);
+        lista.setChoiceMode( AbsListView.CHOICE_MODE_SINGLE );
+
+        //TODO - verificar se conta pertence aquela categoria
+        //Test {
+        Categoria c = new Categoria("Alimentação");
+        c.contas.add(new Conta("conta teste", new Date(), 20.0, true));
+        adapter.add(c);
+        adapter.notifyDataSetChanged();
+        // } Test
+
+        lista.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent,
+                                           View view, int position, long id) {
+                Categoria c = categorias.get( position );
+                if (c.descricao != null) {
+                    Intent it = new Intent(MainActivity.this, TelaConta.class);
+                    it.putExtra("categoria", c);
+                    startActivity( it );
+                }
+                return false;
+            }
+        });
+
+        listagem = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult res) {
+                        if (res.getResultCode() == RESULT_OK) {
+                            Categoria c = (Categoria) res.getData()
+                                    .getSerializableExtra("categoria");
+                            if (posEdicao == -1) {
+                                adapter.add(c);
+                            } else {
+                                categorias.set(posEdicao, c);
+                                adapter.notifyDataSetChanged();
+                            }
+                        }
+                        posEdicao = -1;
+                    }
+                }
+        );
     }
 
     public void adicionar(View v){
@@ -33,5 +98,21 @@ public class MainActivity extends AppCompatActivity {
 
     public void remover(View v){
 
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode,
+                                    @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            Categoria retorno = (Categoria) data.getSerializableExtra("categoria");
+            if (requestCode == 123) {
+                adapter.add(retorno);
+            } else if (requestCode == 234) {
+                categorias.set( posEdicao, retorno);
+                adapter.notifyDataSetChanged();
+                posEdicao = -1;
+            }
+        }
     }
 }
--- app/src/main/java/com/example/controle_de_contas/TelaConta.java
+++ app/src/main/java/com/example/controle_de_contas/TelaConta.java
@@ -1,7 +1,13 @@
 package com.example.controle_de_contas;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AbsListView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,7 +15,15 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.Date;
+import java.util.LinkedList;
+
 public class TelaConta extends AppCompatActivity {
+    LinkedList<Conta> contas;
+    ArrayAdapter<Conta> adapter;
+    ListView lista;
+    EditText edDescricao, edValor, edVencimento;
+    TextView titulo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +35,24 @@ public class TelaConta extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        titulo = findViewById(R.id.txt_titulo);
+        edDescricao = findViewById(R.id.ed_descricao);
+        edValor = findViewById(R.id.ed_valor);
+        edVencimento = findViewById(R.id.ed_data);
+
+        contas = new LinkedList<>();
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, contas);
+
+        lista = findViewById(R.id.lista_contas);
+        lista.setAdapter(adapter);
+        lista.setChoiceMode( AbsListView.CHOICE_MODE_SINGLE );
+
+        Categoria c = (Categoria) getIntent().getSerializableExtra("categoria");
+
+        if (c != null) {
+            titulo.setText(c.descricao);
+            adapter.addAll(c.contas);
+        }
     }
 
     public void adicionar(View v){
@@ -28,7 +60,21 @@ public class TelaConta extends AppCompatActivity {
     }
 
     public void voltar(View v){
+        Categoria categoria = new Categoria(edDescricao.getText().toString());
+        double valor = 0;
+        if(!edValor.getText().toString().isEmpty()){
+             valor = Double.parseDouble(edValor.getText().toString());
+        }
+        Date vencimento = new Date();
+        if(!edVencimento.getText().toString().isEmpty()){
+            vencimento = new Date(edVencimento.getText().toString());
+        }
 
+        Conta c = new Conta(edDescricao.getText().toString(), vencimento, valor,false);
+        Intent resposta = new Intent();
+        resposta.putExtra("conta", c);
+        setResult(RESULT_OK, resposta);
+        finish();
     }
 
 }
